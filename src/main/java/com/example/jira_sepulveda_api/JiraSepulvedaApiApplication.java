f38659d44055efd106515407048d6e502ba7ad4c
Sebastian Sepulveda <jusesemo1@gmail.com>
2025-09-08 13:47:28 -0500

    Frist commit op

---
+++ src/main/java/com/example/jira_sepulveda_api/JiraSepulvedaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jira_sepulveda_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraSepulvedaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraSepulvedaApiApplication.class, args);
+	}
+
+}

17252ad466d7f6234ec09b4520706edd3fe307ab
Sebastian Sepulveda <jusesemo1@gmail.com>
2025-09-10 14:24:07 -0500

    Se agrega un nuevo Get mapping saludo y despedida y se modifica la ruta del @requestmapping, se agrego un nuevo texto cuando se enciende el servidor

--- src/main/java/com/example/jira_sepulveda_api/JiraSepulvedaApiApplication.java
+++ src/main/java/com/example/jira_sepulveda_api/JiraSepulvedaApiApplication.java
@@ -3,11 +3,13 @@ package com.example.jira_sepulveda_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Indica que es una aplicacion de Spring Boot
 public class JiraSepulvedaApiApplication {
 
 	public static void main(String[] args) {
+		// Springaplication.run hace que se escuche un puerto ( Por defecto el 8080 ) //
 		SpringApplication.run(JiraSepulvedaApiApplication.class, args);
+		System.out.println(" EL servidor sprint boot esta corriendo");
 	}
 
 }
--- src/main/java/com/example/jira_sepulveda_api/controller/TestController.java
+++ src/main/java/com/example/jira_sepulveda_api/controller/TestController.java
@@ -1,13 +1,22 @@
 package com.example.jira_sepulveda_api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Este @RestController indica que es un controlador y define las rutas
 @RestController
+@RequestMapping("/api/Supersepol")
 public class TestController {
 
-    @GetMapping("/api/test/saludo")
+    @GetMapping("/saludo")
     public String saludo() {
-        return "Hola desde el endpoint local!";
+        return "Enaaaa hpta!";
+    }
+    @GetMapping("/despedida")
+    public String despedida() {
+        return "Vemos fap";
+    
+
     }
 }

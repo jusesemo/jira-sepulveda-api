bca10b1c02f609265470e01fd359f9691c5e6957
Sebastian Sepulveda <jusesemo1@gmail.com>
2025-11-26 10:55:50 -0500

    Ultimito

--- src/main/java/com/example/jira_sepulveda_api/exception/InvalidUserException.java
+++
@@ -1,7 +0,0 @@
-package com.example.jira_sepulveda_api.exception;
-
-public class InvalidUserException extends RuntimeException {
-    public InvalidUserException(String message) {
-        super(message);
-    }
-}
